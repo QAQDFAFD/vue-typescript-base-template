ba4a064a59724888c02a8e49a757dc81d570f56c
QAQDFAFD <wy15195382735@163.com>
2023-06-29 17:39:20 +0800

    fix: style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,16 +9,37 @@
             <div class="pic"></div>
         </div>
         <div class="right c-red">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
+            <div class="package">
+                <div>开发包</div>
+                <li>pinia</li>
+                <li>axios</li>
+                <li>sass</li>
+                <li>vue-router</li>
+                <li>animate.css</li>
+                <li>element-plus</li>
+                <li>echarts</li>
+                <li>iconfont</li>
+                <li>unocss</li>
+                <li>阿里妈妈字体</li>
+            </div>
+            <div class="init">
+                <div>初始化</div>
+                <li>css reset</li>
+                <li>公共组件的编写</li>
+                <li>全局组件的注册</li>
+                <li>vite.config.ts</li>
+                <li>husky</li>
+                <li>commitlint</li>
+                <li>mockjs</li>
+                <li>只允许使用pnpm</li>
+                <li>夜间模式</li>
+                <li>基于token的身份验证</li>
+                <li>打包配置</li>
+                <li>cssrem</li>
+                <li>.env.</li>
+                <li>脚手架</li>
+                <li>README.md</li>
+            </div>
         </div>
         <div class="switch">
             <el-switch v-model="value" active-color="#2F2F2F" inactive-color="#F1F1F1" @change="changeTheme">
@@ -77,6 +98,16 @@ const changeTheme = (states: any) => {
         width: calc(90% - 50vw);
         height: 100%;
         margin-left: 4rem;
+        display: flex;
+        padding-top: 2rem;
+
+        > div > div {
+            font-size: large;
+            font-weight: 600;
+        }
+        .package {
+            margin-right: 2rem;
+        }
 
         li {
             list-style: disc;
